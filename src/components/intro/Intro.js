ce06ae3e967dc436140a6ac51e576cc68f42d5c2
Affan100 <affan_pathan@hotmail.com>
2021-06-10 22:51:49 +0700

    commit 20%

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,13 +1,42 @@
 import './intro.scss'
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
+
 
 const Intro = () => {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer"],
+        });
+    }, []);
+
+
     return (
         <div className='intro' id='intro'>
-            <div class="left">
-                <div className='imgContainer'>
-                    {/* <img src="../portfolio_prof.jpg" alt="" /> */}
+            <div className='left'>
+                <div className='itemContainer'>
+                    <img className="img1" src="assests/portfolio1.png" alt="" />
                 </div>
             </div>
+
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm </h2>
+                    <h1>Affan Pathan</h1>
+                    <h3>Front End <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    {/* <ExpandMoreIcon className="icon" /> */}
+                    <img src="assests/down-icon.png" alt="" />
+                </a>
+            </div>
+
         </div>
     )
 }
