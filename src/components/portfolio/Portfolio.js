61e69a9b04cfe245ab5bf41dde2bf20d1423940e
Affan100 <affan_pathan@hotmail.com>
2021-06-14 14:19:34 +0700

    commit

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,23 @@
-import './portfolio.css';
+import './portfolio.scss';
 
-export default function Portfolio() {
+const Portfolio = () => {
     return (
         <div className='portfolio' id='portfolio'>
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Desing</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+export default Portfolio;
